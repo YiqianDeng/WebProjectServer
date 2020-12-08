15390bbc97e5ee04df4b95885c4bbc56f5676311
ke <yangke@MacBook-Pro.local>
2020-12-08 02:42:24 -0500

    yangkehaoshoushang

--- src/main/java/com/example/projectserver/repositories/ReviewRepository.java
+++ src/main/java/com/example/projectserver/repositories/ReviewRepository.java
@@ -4,13 +4,17 @@ import com.example.projectserver.models.Review;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 //connect database
 public interface ReviewRepository extends CrudRepository<Review, Integer> {
 
-    @Query(value = "select * from reviews where restaurantId =:restaurantId and userName =:userName", nativeQuery = true)
-    public List<Review> findReviewsByRestaurantAndUser(String restaurantId, String userName);
+    @Query(value = "select * from reviews where restaurant_id =:restaurantId and user_name =:userName", nativeQuery = true)
+    public List<Review> findReviewsByRestaurantAndUser( String restaurantId, String userName);
+
+
+
 
 }
